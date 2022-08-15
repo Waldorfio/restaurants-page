b8c8fa6c761ac1b3d03462659f7a13d18c5010c6
Waldorfio <walid.hanifi707@gmail.com>
2022-08-15 19:14:57 +1000

    Create home page via javascript DOM elements

--- src/index.js
+++ src/index.js
@@ -2,30 +2,62 @@ console.clear()
 
 import './style.css';
 
+// A constructor to create menu item objects easily
+function createFood() {
+    return {
+        title: title,
+        img: img,
+        desc: desc,
+    };
+}
+
 function switchHome() {
-    container = document.getElementById('content-page');
-    container.innerHTML = 'This is now the HOME page!';       // Clear the DOM
+    document.getElementById('content-page').innerHTML = '';       // Clears the container div
+    
+    homeContent1 = document.createElement('div');
+    homeContent1.className = 'content-class';
+    homeContent1.innerHTML = 
+    'The best authentic tacos in Australia.</br>'+
+    'Come try our award winning tacos by Chef <b>Ana Sanchez</b> today.'
+
+    homeContent2 = document.createElement('img');
+    homeContent2.src = 'a.png'
+    homeContent2.id = 'chef-img';
+
+    homeContent3 = document.createElement('div');
+    homeContent3.className = 'content-class';
+    homeContent3.innerHTML = '<em>Why not both? - <b>Ana Sanchez</b></em>'
+
+    // Append the child elements to the DOM
+    document.getElementById('content-page').appendChild(homeContent1);
+    document.getElementById('content-page').appendChild(homeContent2);
+    document.getElementById('content-page').appendChild(homeContent3);
+
     return
 }
-home = document.getElementById('home');
+const home = document.getElementById('home');
 home.addEventListener('click', () => switchHome());
 
+
 function switchMenu() {
-    container = document.getElementById('content-page');
-    container.innerHTML = 'This is now the MENU page!';       // Clear the DOM
+    // Clear the DOM
+    document.getElementById('content-page').innerHTML = 'MENU PAGE';
+    
     return
 }
-menu = document.getElementById('menu');
+const menu = document.getElementById('menu');
 menu.addEventListener('click', () => switchMenu());
 
 
 function switchContact() {
-    container = document.getElementById('content-page');
-    container.innerHTML = 'This is now the CONTACTS page!';       // Clear the DOM
+    // Clear the DOM
+    document.getElementById('content-page').innerHTML = 'CONTACTS PAGE';
+
     return
 }
-contact = document.getElementById('contact');
+const contact = document.getElementById('contact');
 contact.addEventListener('click', () => switchContact());
 
+switchHome();
 
 console.log('------')
