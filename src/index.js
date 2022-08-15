5056d5f6eaef3957c7de515735e81bc2a24d23d6
Waldorfio <walid.hanifi707@gmail.com>
2022-08-15 19:13:29 +1000

    Add base tab switching logic and styling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-alert('just testing2...');
+console.clear()
+
+import './style.css';
+
+function switchHome() {
+    container = document.getElementById('content-page');
+    container.innerHTML = 'This is now the HOME page!';       // Clear the DOM
+    return
+}
+home = document.getElementById('home');
+home.addEventListener('click', () => switchHome());
+
+function switchMenu() {
+    container = document.getElementById('content-page');
+    container.innerHTML = 'This is now the MENU page!';       // Clear the DOM
+    return
+}
+menu = document.getElementById('menu');
+menu.addEventListener('click', () => switchMenu());
+
+
+function switchContact() {
+    container = document.getElementById('content-page');
+    container.innerHTML = 'This is now the CONTACTS page!';       // Clear the DOM
+    return
+}
+contact = document.getElementById('contact');
+contact.addEventListener('click', () => switchContact());
+
+
+console.log('------')
